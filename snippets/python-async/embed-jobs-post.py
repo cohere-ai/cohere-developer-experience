7ee9b2142fed957bcd8cf503f184e0c71a296823
CoderHam <hemant@cohere.com>
2024-08-29 17:32:44 -0500

    fix: misc fixes for documentation + wnb config docs

--- snippets/python-async/embed-jobs-post.py
+++ snippets/python-async/embed-jobs-post.py
@@ -6,8 +6,8 @@ co = cohere.AsyncClient("<<apiKey>>")
 
 async def main():
     # start an embed job
-    response = await co.embed_jobs.create(
-        dataset_id=ds.id, input_type="search_document", model="embed-english-v3.0"
+    job = await co.embed_jobs.create(
+        dataset_id="my-dataset-id", input_type="search_document", model="embed-english-v3.0"
     )
 
     # poll the server until the job is complete
--- snippets/python/embed-jobs-post.py
+++ snippets/python/embed-jobs-post.py
@@ -3,8 +3,8 @@ import cohere
 co = cohere.Client("<<apiKey>>")
 
 # start an embed job
-response = co.embed_jobs.create(
-    dataset_id=ds.id, input_type="search_document", model="embed-english-v3.0"
+job = co.embed_jobs.create(
+    dataset_id="my-dataset-id", input_type="search_document", model="embed-english-v3.0"
 )
 
 # poll the server until the job is complete
