ec7d6b804e6a2c6bd8a2ec4bb9bad7e299722329
platform-endpoints <platform-endpoints@cohere.ai>
2024-09-27 08:13:49 +0000

    Add spec changes
    Co-authored-by: billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>

--- snippets/snippets/java/app/src/main/java/DatasetGet.java
+++ snippets/snippets/java/app/src/main/java/DatasetGet.java
@@ -1,13 +1,13 @@
+/* (C)2024 */
 import com.cohere.api.Cohere;
 import com.cohere.api.resources.datasets.types.DatasetsGetResponse;
 
-
 public class DatasetGet {
-    public static void main(String[] args) {
-        Cohere cohere = Cohere.builder().token("<<apiKey>>").clientName("snippet").build();
+  public static void main(String[] args) {
+    Cohere cohere = Cohere.builder().token("<<apiKey>>").clientName("snippet").build();
 
-        DatasetsGetResponse response = cohere.datasets().get("dataset_id");
+    DatasetsGetResponse response = cohere.datasets().get("dataset_id");
 
-        System.out.println(response);
-    }
+    System.out.println(response);
+  }
 }
--- snippets/snippets/java/app/src/main/java/chatpost/Stream.java
+++ snippets/snippets/java/app/src/main/java/chatpost/Stream.java
@@ -10,40 +10,35 @@ import com.cohere.api.types.StreamedChatResponse;
 import java.util.List;
 
 public class Stream {
-    public static void main(String[] args) {
-        Cohere cohere = Cohere.builder().token("<<apiKey>>").clientName("snippet").build();
+  public static void main(String[] args) {
+    Cohere cohere = Cohere.builder().token("<<apiKey>>").clientName("snippet").build();
 
-        Iterable<StreamedChatResponse> response =
-                cohere.chatStream(
-                        ChatStreamRequest.builder()
-                                .message("What year was he born?")
-                                .chatHistory(
-                                        List.of(
-                                                Message.user(
-                                                        ChatMessage.builder()
-                                                                .message("Who discovered gravity?")
-                                                                .build()),
-                                                Message.chatbot(
-                                                        ChatMessage.builder()
-                                                                .message(
-                                                                        "The man who is widely"
-                                                                            + " credited with"
-                                                                            + " discovering gravity"
-                                                                            + " is Sir Isaac"
-                                                                            + " Newton")
-                                                                .build())))
-                                .build());
+    Iterable<StreamedChatResponse> response =
+        cohere.chatStream(
+            ChatStreamRequest.builder()
+                .message("What year was he born?")
+                .chatHistory(
+                    List.of(
+                        Message.user(
+                            ChatMessage.builder().message("Who discovered gravity?").build()),
+                        Message.chatbot(
+                            ChatMessage.builder()
+                                .message(
+                                    "The man who is widely"
+                                        + " credited with"
+                                        + " discovering gravity"
+                                        + " is Sir Isaac"
+                                        + " Newton")
+                                .build())))
+                .build());
 
-        for (StreamedChatResponse chatResponse : response) {
-            if (chatResponse.isTextGeneration()) {
-                System.out.println(
-                        chatResponse
-                                .getTextGeneration()
-                                .map(ChatTextGenerationEvent::getText)
-                                .orElse(""));
-            }
-        }
-
-        System.out.println(response);
+    for (StreamedChatResponse chatResponse : response) {
+      if (chatResponse.isTextGeneration()) {
+        System.out.println(
+            chatResponse.getTextGeneration().map(ChatTextGenerationEvent::getText).orElse(""));
+      }
     }
+
+    System.out.println(response);
+  }
 }
