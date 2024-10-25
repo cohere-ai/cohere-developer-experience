b722296044b56078ddd5ef46c6975431b1c88afc
Billy Trend <billy@cohere.com>
2024-10-25 15:31:11 +0100

    Final config

--- packages/snippet-tester/src/config.ts
+++ packages/snippet-tester/src/config.ts
@@ -17,23 +17,27 @@ export const config: {
             "snippetRoot": (base: string) => path.join(base, "./snippets/node"),
             "glob": "**/*.js"
         },
-        // {
-        //     "name": "java",
-        //     "exec": (file: string) => `java ${file}`,
-        //     "snippetRoot": (base: string) => path.join(base, "./snippets/java"),
-        //     "glob": "**/*.java"
-        // },
-        // {
-        //     "name": "python",
-        //     "exec": (file: string) => `python ${file}`,
-        //     "snippetRoot": (base: string) => path.join(base, "./snippets/python"),
-        //     "glob": "**/*.go"   
-        // },
-        // {
-        //     "name": "golang",
-        //     "exec": (file: string) => `go run ${file}`,
-        //     "snippetRoot": (base: string) => path.join(base, "./snippets/go"),
-        //     "glob": "**/*.go"
-        // }
+        {
+            "name": "java",
+            "exec": (file: string) => {
+                const classPath = file.replace("app/src/main/java/", "").replace(".java", "").split("/").join(".");
+                
+                return `gradle -PmainClass=${classPath} run`
+            },
+            "snippetRoot": (base: string) => path.join(base, "./snippets/java"),
+            "glob": "./app/src/main/java/**/*.java"
+        },
+        {
+            "name": "python",
+            "exec": (file: string) => `python3 ${file}`,
+            "snippetRoot": (base: string) => path.join(base, "./snippets/python"),
+            "glob": "**/*.py"   
+        },
+        {
+            "name": "golang",
+            "exec": (file: string) => `go run ${file}`,
+            "snippetRoot": (base: string) => path.join(base, "./snippets/go"),
+            "glob": "**/*.go"
+        }
     ]
 }
