aabe91da512dc21ec4d3638587aa2c1027e3315d
Billy Trend <billy@cohere.com>
2025-01-15 10:52:35 +0000

    Add autorelease

---
+++ packages/autorelease/src/index.ts
@@ -0,0 +1,99 @@
+import { Octokit } from "@octokit/rest"
+
+const versionMatchRegex = /v?(\d+\.\d+\.\d+)/g
+
+const languages = ["python", "npm", "go"] as const
+
+const sortVersions = (versions: string[]) => {
+    return versions.sort((a, b) => {
+        const [aMajor, aMinor, aPatch] = a.split(".").map(Number)
+        const [bMajor, bMinor, bPatch] = b.split(".").map(Number)
+        
+        if (aMajor !== bMajor) {
+            return aMajor - bMajor
+        } else if (aMinor !== bMinor) {
+            return aMinor - bMinor
+        }
+
+        return aPatch - bPatch
+    })
+}
+
+const getPythonVersions = async () => {
+    const response = await fetch("https://pypi.org/simple/cohere/")
+    const text = await response.text()
+    const matches = text.matchAll(versionMatchRegex)
+    return [...matches].map(m => m[1])
+}
+
+const getNpmVersions = async () => {
+    const response = await fetch("https://registry.npmjs.org/-/package/cohere-ai/dist-tags")
+    const json = await response.json() as { latest: string }
+    return [json.latest]
+}
+
+const getGoVersions = async () => {
+    const response = await fetch("https://proxy.golang.org/github.com/cohere-ai/cohere-go/v2/@latest")
+    const json = await response.json() as { Version: string }
+    return [json.Version.replace("v", "")]
+}
+
+const updateVersion = async (version: string, update: "major" | "minor" | "patch") => {
+    const [major, minor, patch] = version.split(".").map(Number)
+    
+    return ({
+        major: `${major + 1}.0.0`,
+        minor: `${major}.${minor + 1}.0`,
+        patch: `${major}.${minor + 1}.${patch + 1}`,
+    })[update]
+}
+
+const getLatestVersions = async () => {
+    const [pythonVersions, npmVersions, goVersions] = await Promise.all([
+        getPythonVersions(),
+        getNpmVersions(),
+        getGoVersions(),
+    ])
+    return {
+        python: sortVersions(pythonVersions).pop()!,
+        npm: sortVersions(npmVersions).pop()!,
+        go: sortVersions(goVersions).pop()!,
+    }
+}
+
+const getNextVersions = async (update: "major" | "minor" | "patch") => {
+    const latest = await getLatestVersions()
+
+    return {
+        python: {
+            previous: latest.python,
+            next: await updateVersion(latest.python, update),
+        },
+        npm: {
+            previous: latest.npm,
+            next: await updateVersion(latest.npm, update),
+        },
+        go: {
+            previous: latest.go,
+            next: await updateVersion(latest.go, update),
+        },
+    }
+}
+
+const createRelease = (language: typeof languages[number], version: string) => {
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+    
+    octokit.repos.createRelease({
+        owner: process.env.GITHUB_OWNER!,
+        repo: process.env.GITHUB_REPO!,
+        tag_name: `${language}/${version}`,
+        name: `${language}/${version}`,
+        body: `This release updates the ${language} package to ${version}.`,
+    })
+}
+
+(async () => {
+    const nextVersions = await getNextVersions("patch")    
+
+    await Promise.all(languages.map(async language => createRelease(language, nextVersions[language].next)))        
+})()
