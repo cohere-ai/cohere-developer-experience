81a22ddbcb2ac437c76efacf7ea8a10786685396
Max Shkutnyk <max@lightsonsoftware.com>
2025-05-19 23:32:46 +0300

    Addressing coobooks styles issues

--- fern/components/cookbooks/SearchBar.tsx
+++ fern/components/cookbooks/SearchBar.tsx
@@ -12,7 +12,7 @@ export const SearchBar: React.FC<SearchBarProps> = ({ searchQuery, setSearchQuer
       placeholder="Search"
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
-      className="w-full px-4 py-2 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="w-full px-4 py-2 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
     />
     <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="text-gray-400" viewBox="0 0 16 16">
