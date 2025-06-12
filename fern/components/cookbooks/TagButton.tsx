a9c98cd5a1db8ba7c4302bd761028765a5fd5313
Max Shkutnyk <max@lightsonsoftware.com>
2025-06-12 12:42:02 +0300

    adjust styles for dark mode

--- fern/components/cookbooks/TagButton.tsx
+++ fern/components/cookbooks/TagButton.tsx
@@ -8,7 +8,7 @@ interface TagButtonProps {
 
 const tagButtonClass = "px-2 py-1 text-[12px] rounded-full transition-colors cursor-pointer font-normal";
 const selectedTagClass = "bg-[#333293] text-white hover:bg-[#333293]/90";
-const unselectedTagClass = "bg-white text-[#666666] shadow-[0_0_0_1px_#E5E5E5] outline outline-1 outline-[#E5E5E5] hover:bg-[#d4d4f0] hover:text-[#4d4d4d]";
+const unselectedTagClass = "bg-transparent text-[#666666] shadow-[0_0_0_1px_#E5E5E5] outline outline-1 outline-[#E5E5E5] hover:bg-[#d4d4f0] hover:text-[#4d4d4d] dark:dark-theme-text-color";
 
 export const TagButton: React.FC<TagButtonProps> = ({ tag, isSelected, onClick }) => (
   <button
