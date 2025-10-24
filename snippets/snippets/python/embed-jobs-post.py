ccaa0ebcfbfc051bc4d108af916a8aefb84f6e56
platform-endpoints <platform-endpoints@cohere.ai>
2025-10-24 16:08:43 +0000

    Add spec changes
    Co-authored-by: Max Shkutnyk <invader89@gmail.com>

--- snippets/snippets/python/embed-jobs-post.py
+++ snippets/snippets/python/embed-jobs-post.py
@@ -4,7 +4,7 @@ co = cohere.Client()
 
 # start an embed job
 job = co.embed_jobs.create(
-    dataset_id="my-dataset-id", input_type="search_document", model="embed-v4.0"
+    dataset_id="my-dataset-id", input_type="search_document", model="embed-english-v3.0"
 )
 
 # poll the server until the job is complete
