c1d9dabbc2ce083de928eef7fc6f02e96a7aad1b
platform-endpoints <platform-endpoints@cohere.ai>
2026-03-27 13:04:45 +0000

    Add spec changes
    Co-authored-by: Michael <michael@cohere.com>

--- snippets/snippets/node/transcriptions-post.js
+++ snippets/snippets/node/transcriptions-post.js
@@ -1,17 +1,16 @@
-import { CohereClient } from 'cohere-ai';
+import { CohereClientV2 } from 'cohere-ai';
+import fs from 'fs';
 
-const fs = require('fs');
-
-const cohere = new CohereClient({});
+const cohere = new CohereClientV2({});
 
 (async () => {
-  const transcription = await cohere.audio.transcriptions.create(
-    fs.createReadStream('./sample.wav'),
-    {
-      model: 'cohere-transcribe-03-2026',
-      language: 'en',
-    }
-  );
+  const fileBuffer = fs.readFileSync('./sample.wav');
+  const file = new File([fileBuffer], 'sample.wav');
 
-  console.log(transcription);
+  const response = await cohere.audio.transcriptions.create(file, {
+    model: 'cohere-transcribe-03-2026',
+    language: 'en',
+  });
+
+  console.log(response.text);
 })();
