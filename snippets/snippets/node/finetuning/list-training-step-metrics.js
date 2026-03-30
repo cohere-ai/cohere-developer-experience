40f3fd11b9fd6b445b024a2fcf6f2e67f6181147
platform-endpoints <platform-endpoints@cohere.ai>
2026-03-30 13:46:32 +0000

    Add spec changes
    Co-authored-by: cohere-renovate[bot] <143127836+cohere-renovate[bot]@users.noreply.github.com>

--- snippets/snippets/node/finetuning/list-training-step-metrics.js
+++ snippets/snippets/node/finetuning/list-training-step-metrics.js
@@ -3,9 +3,8 @@ import { CohereClient } from 'cohere-ai';
 const cohere = new CohereClient({});
 
 (async () => {
-  const trainingStepMetrics = await cohere.finetuning.listTrainingStepMetrics(
-    'test-finetuned-model-id'
-  );
+  const trainingStepMetrics =
+    await cohere.finetuning.listTrainingStepMetrics('test-finetuned-model-id');
 
   console.log(trainingStepMetrics);
 })();
